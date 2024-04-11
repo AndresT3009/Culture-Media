e47d189f4ffda6d63ca72797a1e3b73a5e0289d8
Darwin <darwin.a1120@gmail.com>
2024-04-10 20:46:56 -0500

    Adding video structures

---
+++ src/main/java/culturemedia/model/Video.java
@@ -0,0 +1,47 @@
+package culturemedia.model;
+
+public class Video {
+
+    private String codigo;
+
+    private String titulo;
+
+    private String descripcion;
+
+    private String Double;
+
+    public Video() {
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDouble() {
+        return Double;
+    }
+
+    public void setDouble(String aDouble) {
+        Double = aDouble;
+    }
+}
