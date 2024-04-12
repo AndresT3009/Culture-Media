3f6a1ea9c6b81d6e8eeca9628c6e2cefe14e8cb6
Darwin <darwin.a1120@gmail.com>
2024-04-12 18:51:21 -0500

    Adding exceptions

---
+++ src/main/java/culturemedia/exceptions/VideoNotFoundException.java
@@ -0,0 +1,12 @@
+package culturemedia.exceptions;
+
+public class VideoNotFoundException extends CulturemediaException{
+
+
+    public VideoNotFoundException (String message){super(message);}
+
+
+    public VideoNotFoundException (){};
+
+
+}
